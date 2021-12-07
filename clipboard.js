8ea519afbd4c0356c7f405cf2d8c4ada2201bca8
chrisws <cwarrensmith@gmail.com>
2021-12-07 21:44:28 +1000

    Added clipboard button for PRE text

---
+++ clipboard.js
@@ -0,0 +1,24 @@
+function copyText(code, text) {
+  return function() {
+    navigator.clipboard.writeText(text);
+    var color = code.style.borderColor;
+    code.style.borderColor = "#a2a9b1";
+    window.setTimeout(function() {
+      code.style.borderColor = color;
+    }, 250);
+  }
+}
+
+window.onload = function() {
+  for (var code of document.getElementsByTagName("pre")) {
+    var button = document.createElement('button');
+    var text = document.createElement("span");
+    text.innerHTML = "&#x1F4CB;";
+    button.appendChild(text);
+    button.style.float="right";
+    button.style.border="0";
+    button.title = "Copy to clipboard";
+    button.onclick = copyText(code, code.childNodes[0].innerHTML);
+    code.insertBefore(button, code.childNodes[0]);
+  }
+}
